4f182f5e5da083423b3999be3b3d03ae3e2d290c
a17364 <a17364@d1-08-06.dped.local>
2023-02-13 16:24:05 +0000

    Alteração requisitada da Ficha de Trabalho 5

--- src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
@@ -1,6 +1,34 @@
 package psi_11_o.m17b_fichatrabalho5;
 
 public class Ponto2D {
+
+    /**
+     * @return o valor do atributo x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x dá set ao valor de x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return o valor do atributo x
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y dá set ao valor de x
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
     private int x;
     private int y;
     
@@ -15,11 +43,11 @@ public class Ponto2D {
     }
     
     public double distancia(Ponto2D a){
-        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
+        return Math.abs(Math.sqrt(Math.pow((getX()-a.getX()),2)+Math.pow((getY()-a.getY()),2)));
     }
 
     @Override
     public String toString() {
-        return "Ponto2D[" + x + "," + y + "]";
+        return "Ponto2D[" + getX() + "," + getY() + "]";
     }    
 }
